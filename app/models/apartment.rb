e1892fbfecc671daf1574ff7e62e51f6fa371f7a
DeeMo44 <demario326@gmail.com>
2023-05-09 14:04:32 -0400

    validations complete

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,3 +1,4 @@
 class Apartment < ApplicationRecord
   belongs_to :user
+  validates :street, :city, :unit, :state, :square_footage, :price, :bedrooms, :bathrooms, :pets, :image, presence: true
 end
